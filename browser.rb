7ca9c1da316c6490bc0a548c0fe4906fe04e0bb4
Hubert Pompecki <hpompecki@gmail.com>
2015-12-05 18:24:41 +0000

    implement POST requests in the browser and the server

--- browser.rb
+++ browser.rb
@@ -1,12 +1,36 @@
 require 'socket'
+require 'json'
 
 host = 'localhost'
 port = 2000
 path = "/index.html"
 
-request = "GET #{path} HTTP/1.0\r\n\r\n"
-
 socket = TCPSocket.open(host, port)
+
+puts "Which request do you want to send?"
+puts "1) GET"
+puts "2) POST"
+user_input = gets.chomp
+
+case user_input
+when "1"
+	request = "GET #{path} HTTP/1.0\r\n\r\n"
+when "2"
+	puts "Let's pretend we are registering a viking for a raid."
+	print "Please enter the vikings name: "
+	name = gets.chomp
+	print "Please enter the vikings email: "
+	email = gets.chomp
+	params = {viking: {name: name, email: email}}
+	request =  "POST /thanks.html HTTP/1.0\r\n"
+	request << "Content-Length: #{params.to_json.length}\r\n"
+	request << "\r\n"
+	request << params.to_json
+else
+	puts "It is not a valid choice!"
+	exit
+end
+ 
 socket.print request
 response = socket.read
 
--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'json'
 
 server = TCPServer.open(2000)
 loop do
@@ -12,6 +13,18 @@ loop do
 	if header =~ /GET/ && header =~ /\/index.html/
       client.print "HTTP/1.0 200 OK \r\n\r\n"
       client.print File.read("index.html")
+	elsif header =~ /POST/ && header =~ /\/thanks.html/
+		# find the length of body
+		content_length = /Content-Length:\s+(\d+)/.match(header)[1].to_i
+		# read body and convert from JSON to hash
+		body = client.read(content_length)
+		params = JSON.parse(body)
+		# prepare response body
+		response_body = File.read("thanks.html").
+			gsub!("<%= yield %>", "<li>Name: #{params["viking"]["name"]}</li><li>Email: #{params["viking"]["email"]}</li>")
+		# send the response
+		client.print "HTTP/1.0 200 OK\r\n\r\n"
+		client.print response_body			
 	else  
 	  client.print "HTTP/1.0 404 NOT FOUND\r\n\r\n"
 	end
